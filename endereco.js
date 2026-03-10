31f4ee17c211fd4fb788348708096f08370bdac5
borgessantosadrian-sudo <borges.santos.adrian@escola.pr.gov.br>
2026-03-10 14:40:52 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,9 @@
+console.log("endereço completo")
+
+let rua = ("rua das flores");
+let numero = 489;
+let cidade = "curitiba";
+
+let endereco = rua +", " + numero +" - " cidade;
+console.log(endereco)
+console.log("o enderoço coerreto é: " + endereco);
