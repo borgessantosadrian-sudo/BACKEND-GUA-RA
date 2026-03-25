e13d2433d5711ed964aa581ae3fc1727ab2fd2cb
borgessantosadrian-sudo <borges.santos.adrian@escola.pr.gov.br>
2026-03-25 18:28:20 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,9 @@
+const produto = "mochila impermeável"
+let preco = 189.90;
+let estoque = 14
+const categoria = "acessório";
+
+console.log(`o produto é: ${produto}.`);
+console.log(`a ${produto} custa R${preco.toFixed(2)}.`);
+console.log(`o estoque ainda possui ${estoque} mochilas.`)
+console.log(`a categoria de ${produto} é ${categoria}.`)
