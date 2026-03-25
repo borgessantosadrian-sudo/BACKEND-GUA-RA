a51d43f98ef4418a39481a1e70ba1731009a0263
borgessantosadrian-sudo <borges.santos.adrian@escola.pr.gov.br>
2026-03-25 18:52:37 +0000

    versao3

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,6 @@ let tempCelsius = 18.5;
 
 const tempFahrenheit = tempCeslsius * (9/5) + 32;
 const tempKelvin = tempCeslsius + 273.15;
+console.log(`aA temperaturade ${cidade} está ${tempCelsius} Cº`);
+console.log(`fahrenheit: ${tempFahrenheit.toFixed(1)} fº`);
+console.log(`kelvin: ${tempKelvin.toFixed(1)}kº`);
