b8ec0845141ebfef632775aac54dab73fa27771d
borgessantosadrian-sudo <borges.santos.adrian@escola.pr.gov.br>
2026-04-14 17:34:22 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,8 @@
+function subirDeNivel(nivelAtual){
+    return nivelAtual + 1;
+};
+let meuNivel = 10;
+let nivelAtualizado = subirDeNivel(meuNivel);// somando nivel atual + 1
+//                  (nivelAtual + 1) + 10]
+
+console.log("seu nivel atuael é: " + NivelAtualizado);
