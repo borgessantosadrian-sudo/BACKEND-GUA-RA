853d9702574f43f5ee8e84ea4b9c72cfd1e95784
borgessantosadrian-sudo <borges.santos.adrian@escola.pr.gov.br>
2026-05-11 19:11:01 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,11 @@
+let idade = 15;
+let nome = "adrian";
+let serie = "1-A";
+let nascimento = 2009;
+let cpf = "984.598.987.45";
+console.log("Idade", idade);
+console.log("Nome", nome);
+console.log("Serie", serie);
+console.log("Nascimento", nascimento);
+console.log("CPF", cpf)
+console.log(`Nome: ${nome}, Idade: ${idade}, Série: ${serie}, Ano de nascimento: ${nascimento}, CPF: ${cpf}. você concluiu o cadastro`);
