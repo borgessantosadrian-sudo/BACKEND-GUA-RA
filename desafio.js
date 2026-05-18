163be430c4a2d7a288035f897cc4169ba12e9e52
borgessantosadrian-sudo <borges.santos.adrian@escola.pr.gov.br>
2026-05-18 19:22:53 +0000

    versao2

--- desafio.js
+++ desafio.js
@@ -9,3 +9,26 @@ console.log("Serie", serie);
 console.log("Nascimento", nascimento);
 console.log("CPF", cpf)
 console.log(`Nome: ${nome}, Idade: ${idade}, Série: ${serie}, Ano de nascimento: ${nascimento}, CPF: ${cpf}. você concluiu o cadastro`);
+
+
+
+let nome = "Erick";
+let idade = 16;
+let nascimento = 2010;
+let cpf = "122.155.134-14";
+let nota1 = 7.0;
+let nota2 = 8.0;
+let primeiroTri = nota1 + nota2;
+let segundoTri = nota1 * nota2;
+let terceiroTri = nota2 - nota1;
+
+console.log("Nome:", nome);
+console.log("idade:", idade);
+console.log("nascimento:", nascimento);
+console.log("CPF:", cpf);
+console.log("nota1", nota1);
+console.log("nota2", nota2);
+console.log(primeiroTri)
+console.log(segundoTri)
+console.log(terceiroTri)
+console.log(`Nome: ${nome}, Idade: ${idade}, Ano de nascimento: ${nascimento}, CPF: ${cpf}. você concluiu o cadastro, nota = ${primeiroTri}, ${segundoTri} e ${terceiroTri} `);
